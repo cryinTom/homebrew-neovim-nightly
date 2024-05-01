4fdac6f194c8d2ff24de4efed5431be56f8e4003
Andrey <47274490+cryinTom@users.noreply.github.com>
2024-05-01 17:15:33 +0300

    update link

--- Casks/neovim-nightly.rb
+++ Casks/neovim-nightly.rb
@@ -3,7 +3,8 @@ cask "neovim-nightly" do
   sha256 :no_check
 
   arch arm: "arm64", intel: "x86_64"
-  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-#{arch}.tar.gz",
+  url "https://github.com/neovim/neovim/releases/download/nightly/nvim-macos-arm64.tar.gz",
+
       verified: "github.com/neovim"
 
   name "Neovim"
@@ -12,7 +13,7 @@ cask "neovim-nightly" do
 
   conflicts_with formula: "neovim"
 
-  binary "nvim-macos-#{arch}/bin/nvim"
+  binary "nvim-macos-arm64/bin/nvim"
 
   postflight do
     system_command "xattr", args: ["-cr", "#{staged_path}"]
